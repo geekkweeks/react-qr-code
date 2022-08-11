50be0d7938fc204bd11bb5b7644945a906bd8ffb
geekkweek <zahgreat30@gmail.com>
2022-08-11 20:24:01 +0700

    detect video

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     useEffect(() => {
         async function enableStream() {
             try {
-                const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
                 alert('Camera found')
             } catch (err) {
                 alert('Camera not found')
@@ -15,19 +15,7 @@ function App() {
     }, []);
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <button className="">Click me</button>
         </div>
     );
 }
